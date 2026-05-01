c54a4c8fd171964a1ec3cce0bca56b77370e9d7a
lanaalhabbal <lanaalhabbal173@gmail.com>
2026-05-01 15:26:52 +0300

    create studycompanionscreen and controller

--- flutter/test/lib/view/screen/home/study_companion_screen.dart
+++ flutter/test/lib/view/screen/home/study_companion_screen.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/widgets.dart';
+import 'package:get/get_state_manager/src/simple/get_view.dart';
+import 'package:test/controller/home/study_companion_controller.dart';
 
-class StudyCompanionScreen extends StatelessWidget {
+class StudyCompanionScreen extends GetView<StudyCompanionController> {
   const StudyCompanionScreen({super.key});
 
   @override
